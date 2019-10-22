ebb63fc5c2e6349bbeccd0106c5ba36b4c33940f
zong sheng lian <2214057556@qq.com>
2019-10-22 23:54:16 +0800

    客户端

---
+++ 201727010509连宗胜/Client/cn/edu/scau/cmi/lianzongsheng/client/consistent/Client.java
@@ -0,0 +1,24 @@
+package cn.edu.scau.cmi.lianzongsheng.client.consistent;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Client extends Application{
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane pane = FXMLLoader.load(getClass().getResource("/cn/edu/scau/cmi/lianzongsheng/consistent/View.fxml"));
+			Scene scene = new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
