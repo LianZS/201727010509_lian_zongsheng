f20bae4051ffd3902671408639a6765649170bad
LianZS <2214057556@qq.com>
2019-11-16 19:58:08 +0800

    Hibernate

---
+++ 201727010509连宗胜/Client/cn/edu/scau/cmi/lianzongsheng/client/domain/MainClass.java
@@ -0,0 +1,10 @@
+package cn.edu.scau.cmi.lianzongsheng.client.domain;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		new SqlConnection().TheSqlConnection();
+	}
+}
